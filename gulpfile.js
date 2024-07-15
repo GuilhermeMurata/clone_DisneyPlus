b6bc5c0e0579c0129fc86b4d932a102c7ea4ac8a
Guilherme Murata <123330836+GuilhermeMurata@users.noreply.github.com>
2024-07-15 11:24:53 -0300

    Adding compress and watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require ('sass'));
 
-function testeGulp() {
-    console.log("Ola mundo");
-    cb();
+function styles() {
+    return gulp.src('./src/styles/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = testeGulp;
+exports.default = styles;
+exports.watch = function() {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+}
