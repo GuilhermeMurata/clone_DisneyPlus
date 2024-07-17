3008a1c9ca4a837ad6165768dc4a962a08b751d4
Guilherme Murata <123330836+GuilhermeMurata@users.noreply.github.com>
2024-07-17 15:04:03 -0300

    Add button EventListener

--- src/main.js
+++ src/main.js
@@ -3,7 +3,28 @@ document.addEventListener('DOMContentLoaded', function() {
     
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
-            console.log(botao.target);
+            const abaAlvo = botao.target.dataset.tabButton;
+            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
+            hideAllTabs();
+            aba.classList.add('shows__list--is-active');
+            removeBotaoAtivo();
+            botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
 })
+
+function removeBotaoAtivo() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('shows__tabs__button--is-active');
+    }
+}
+
+function hideAllTabs() {
+    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+
+    for (let i = 0; i < tabsContainer.length; i++) {
+        tabsContainer[i].classList.remove('shows__list--is-active');
+    }
+}
